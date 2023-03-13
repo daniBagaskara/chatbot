7a6303f0b2623474920700234c1156c7ddd826b1
dani_bagaskara <dani_bagaskara@quick.com>
2023-03-13 14:09:06 +0700

    update database

--- controller/App.js
+++ controller/App.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const jwt = require('jsonwebtoken');
-const { env } = require('process');
-require('dotenv').config();
 const {Configuration, OpenAIApi} = require('openai');
+const mysql = require('mysql2');
+require('dotenv').config();
 
 class App {
     index(req, res) {
@@ -23,7 +23,7 @@ class App {
         const token = jwt.sign({
             email: req.body.username,
             userId: req.body.token,
-        }, process.env.jwt_secret_key, { expiresIn: '4m' });
+        }, process.env.jwt_secret_key, { expiresIn: '5m' });
 
         console.log(token);
         res.cookie('token', token, { httpOnly: true, maxAge: 5 * 60 * 1000 });
@@ -44,7 +44,26 @@ class App {
     }
 
     prompt(req, res){
+        // konfigurasi koneksi ke MySQL
+        var connection = mysql.createConnection({
+            host: process.env.db_host,
+            user: process.env.db_user,
+            password: process.env.db_pwd,
+            database: process.env.db_name,
+            port:3306
+        });
+
         const prompt = req.body.message;
+
+        if(prompt.length < 4 ){
+            connection.query(
+                'SELECT response_api FROM response_chatbot WHERE id = 3',
+                function(err, results, fields) {
+                  console.log(results);
+                  return res.json(results[0].response_api);
+                }
+            );
+        }
         const configuration = new Configuration({
             apiKey: process.env.OPENAI_API_KEY,
         });
@@ -52,20 +71,29 @@ class App {
 
         const response = openai.createCompletion({
             model: "text-davinci-003",
-            prompt:prompt,
+            prompt: prompt,
             temperature: 0.9,
             max_tokens: 150,
             top_p: 1,
             frequency_penalty: 0,
             presence_penalty: 0.6,
             stop: [" Human:", " AI:"],
-          });
+        });
 
         response.then(upcomingData => {
             console.log(prompt);
             console.log(upcomingData.data);
             res.json(upcomingData.data.choices[0].text);
-        })
+            const sql = `INSERT INTO response_chatbot (prompt, response_api) VALUES (?, ?)`;
+            const values = [prompt,upcomingData.data.choices[0].text];
+
+            connection.query(sql, values,(error, results, fields) => {
+                if (error) throw error;
+                console.log(`Response for prompt '${prompt}' has been saved to MySQL.`);
+            });
+
+            connection.end();
+        });
     }
 
     destroy(req,res){
